87351da622c9b9d8eb7062c4aee1bcdb7fd1b25a
Yana DEREVIANKO <79232031+yanaderevianko726@users.noreply.github.com>
2023-05-26 04:16:45 +0700

    update face detector

--- detect_face_video.py
+++ detect_face_video.py
@@ -1,19 +1,19 @@
-import cv2
+import cv2 as cv
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-cap = cv2.VideoCapture(0)
+face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+vs = cv.VideoCapture(0)
 
 while True:
-    _, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, img = vs.read()
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        cv.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    cv2.imshow('img', img)
-    k = cv2.waitKey(30) & 0xff
+    cv.imshow('img', img)
+    k = cv.waitKey(30) & 0xff
     if k==27:
         break
         
-cap.release()
+vs.release()
